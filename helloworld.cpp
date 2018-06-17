338e50931fd36eb20cc4820e772fbe019bb8fca3
Jeremy Aguilon <jeraguilon@gmail.com>
2018-06-17 16:02:35 -0700

    Inheritance

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,6 +8,13 @@ using namespace std;
 /*     double* elem; */
 /* }; */
 
+class Container {
+    public:
+        virtual double& operator[](int) = 0; // pure virtual, must be defined
+        virtual int size() const = 0;
+        virtual ~Container() {}
+};
+
 class Vector {
     public:
         Vector(int s) { 
@@ -17,12 +24,32 @@ class Vector {
             elem = new double[s];
             sz = s;
         }
+        Vector(std::initializer_list<double> lst): elem{new double[lst.size()]}, sz{static_cast<int>(lst.size())} {
+           copy(lst.begin(), lst.end(), elem); 
+        }
+
+
         ~Vector() { delete[] elem; };
-        double& operator[](int i){ return elem[i]; }
-        int size() { return sz; }
+
+        double& operator[](int i){ 
+            return elem[i];
+        }
+        int size() const { return sz; }
+
+        Vector push_back(double);
     private:
         double* elem;
         int sz;
+};
+
+class Vector_container: public Container {
+    Vector v;
+    public:
+    Vector_container(int s): v(s) {}
+    ~Vector_container() {}
+
+    double& operator[](int i) { return v[i]; }
+    int size() const { return v.size(); }
 };
 
 double read_and_sum(int);
@@ -33,7 +60,7 @@ int main() {
 }
 
 double read_and_sum(int s) {
-    Vector v{s};
+    Vector v(s);
     for (int i = 0; i < s; i++) {
         cin >> v[i];
     }
