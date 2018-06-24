3cacdc73621b0a0845d4712d04e0a3659d4d31f9
Jeremy Aguilon <jeraguilon@gmail.com>
2018-06-24 16:06:48 -0700

    Bug fixed

--- treeperimeter/treeperimeter.cc
+++ treeperimeter/treeperimeter.cc
@@ -28,61 +28,57 @@ void Tree::printPerimeter() {
     stack<node*> stack{};
     
     node* curr = root;
-    while (curr != NULL) {
-        cout << curr->val;
+    while (curr != NULL && curr->left != NULL) {
+        cout << curr->val << "\n";
         curr = curr->left;
     }
 
     stack.push(root);
     while (!stack.empty()) {
-        //cout << "TRAVERSING 1";
         node* curr = stack.top();
         if (curr->left == NULL && curr->right == NULL) {
-            cout << curr->val;
+            cout << curr->val<<"\n";
         }
+        stack.pop();
         if (curr->right != NULL) stack.push(curr->right);
         if (curr->left != NULL) stack.push(curr->left);
-        stack.pop();
     }
 
-    curr = root;
-    while (curr != NULL) {
-        stack.push(root);
+    curr = root->right;
+    while (curr != NULL && curr->right != NULL) {
+        stack.push(curr);
         curr = curr->right;
     }
     while(!stack.empty()) {
-        //cout << "TRAVERSING 2";
-        cout << stack.top()->val;
+        cout << stack.top()->val << "\n";
         stack.pop();
     }
 }
 
 int main() {
-   Tree tree{}; 
+    Tree tree{}; 
 
-   node* root = new node;
-   node* root_l = new node;
-   node* root_r = new node;
+    node* root = new node;
+    node* root_l = new node;
+    node* root_r = new node;
 
-   node* root_ll = new node;
-   node* root_lr = new node;
-   
-   node* root_rr = new node;
-    
-   root->val = 0;
-   root_l->val = 1;
-   root_r->val = 5;
-   root_ll->val = 2;
-   root_lr->val = 3;
-   root_rr->val = 4;
+    node* root_ll = new node;
+    node* root_lr = new node;
 
-   root->left = root_l;
-   root->right = root_r;
-   root_l->left = root_ll;
-   root_l->right = root_lr;
-   root_r->right = root_rr;
-   tree.root = root;
+    node* root_rr = new node;
 
-   cout << true<<"\n";
-   cout << (root_ll->left == NULL)<<"\n";
+    root->val = 0;
+    root_l->val = 1;
+    root_r->val = 5;
+    root_ll->val = 2;
+    root_lr->val = 3;
+    root_rr->val = 4;
+
+    root->left = root_l;
+    root->right = root_r;
+    root_l->left = root_ll;
+    root_l->right = root_lr;
+    root_r->right = root_rr;
+    tree.root = root;
+    tree.printPerimeter();
 }
