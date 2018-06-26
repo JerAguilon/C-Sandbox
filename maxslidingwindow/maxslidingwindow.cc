f37501321991665c596ebd0fc0fee030d9b3e8ef
Jeremy Aguilon <jeraguilon@gmail.com>
2018-06-25 23:32:14 -0700

    Commented solution

--- maxslidingwindow/maxslidingwindow.cc
+++ maxslidingwindow/maxslidingwindow.cc
@@ -13,11 +13,28 @@ vector<string> find_max_sliding_window(vector<int>& v, int window_size) {
     deque<int> dq;
 
     int i = 0;
+    
+    /**
+     * First, create your window by iterating through. Usng a deque, you want the
+     * first element to always be the index of local max. In addition, we get rid of
+     * invalid candidates by making sure that the list is strictly decreasing.
+     */
     for ( ; i < window_size; i++) {
         while (!dq.empty() && v[i] >= v[dq.back()])
             dq.pop_back();
         dq.push_back(i);
     }
+    /**
+     * Now we can perform almost the same logic, but we need to remove the head
+     * when the index is out of bounds of the window as well.
+     *
+     * OVERALL RUNTIME: O(n). There's a nested loop, but note that each element
+     * is added to the list at most once and removed to the list at most once.
+     *
+     * Really this is similar to many sliding window problems, except you need
+     * a clever data structure to represent state. Most "common" problems
+     * rely on tricky pointers, but no internal data structure.
+     */
     for ( ; i < v.size() ; i++) {
         maximums.push_back(v[dq.front()]);
 
