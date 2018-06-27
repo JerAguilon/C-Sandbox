7b351f1dde80941a9f6e7201eb16b3aef7bed511
Jeremy Aguilon <jeraguilon@gmail.com>
2018-06-26 19:54:03 -0700

    Reverse linked lits

---
+++ reverselinkedlist/reverselinkedlist.cc
@@ -0,0 +1,28 @@
+struct LinkedListNode {
+    public:
+        LinkedListNode* next;
+};
+
+LinkedListNode* reverse_recursive(
+        LinkedListNode* head) {
+    if (head == nullptr || head->next==nullptr) return head;
+    LinkedListNode* newHead = reverse_recursive(head->next);  
+    head->next->next = head;
+    head->next = nullptr;
+    return newHead;
+}
+
+LinkedListNode* reverse_iterative(LinkedListNode* head) {
+    if (head == nullptr || head->next == nullptr) return head;
+
+    LinkedListNode* newHead = head;
+    LinkedListNode* curr = head->next;
+    newHead->next = nullptr;
+    while (curr != nullptr) {
+        LinkedListNode* nextCurr = curr->next;
+        curr->next = newHead;
+        newHead = curr;
+        curr = nextCurr;
+    }
+    return newHead;
+}
